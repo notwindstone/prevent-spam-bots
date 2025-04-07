fea961d70b9dd890077ba135446bf356046127aa
notwindstone <notwindstone@gmail.com>
2025-04-07 16:42:52 +0300

    feat: nah no way it works

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,13 @@
 import env from "env-var";
+import { config as denoConfig } from 'https://deno.land/x/dotenv/mod.ts';
+await denoConfig({ export: true });
 
 export const config = {
     NODE_ENV: env
         .get("NODE_ENV")
         .default("development")
         .asEnum(["production", "test", "development"]),
-    BOT_TOKEN: Deno.env.get("BOT_TOKEN").required().asString(),
+    BOT_TOKEN: Deno.env.get("BOT_TOKEN"),
 
     LOCK_STORE: env.get("LOCK_STORE").default("memory").asEnum(["memory"]),
 };
