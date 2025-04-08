1825a262e288ed8e4b6699c389889ca0ca33739e
notwindstone <notwindstone@gmail.com>
2025-04-08 12:48:21 +0300

    feat: save code

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 import env from "env-var";
-import { config as denoConfig } from 'https://deno.land/x/dotenv/mod.ts';
-await denoConfig({ export: true });
 
 export const config = {
     NODE_ENV: env
         .get("NODE_ENV")
         .default("development")
         .asEnum(["production", "test", "development"]),
-    BOT_TOKEN: Deno.env.get("BOT_TOKEN"),
+    BOT_TOKEN: env.get("BOT_TOKEN"),
 
     LOCK_STORE: env.get("LOCK_STORE").default("memory").asEnum(["memory"]),
 };
